4ee591077553521e7f68c9e8dc04f3145bc1c458
Andy Potanin <andy.potanin@usabilitydynamics.com>
2014-05-09 01:05:42 -0400

    wip

--- lib/class-api.php
+++ lib/class-api.php
@@ -32,23 +32,93 @@ namespace UsabilityDynamics {
     public static $version = '0.1.2';
 
     /**
+     * API Namespace
+     *
+     * @public
+     * @static
+     * @property $namespace
+     * @type {String}
+     */
+    public static $namespace = null;
+
+    /**
+     * Routes
+     *
+     * @public
+     * @static
+     * @property $routes
+     * @type {Object}
+     */
+    public static $_routes = Array();
+
+    /**
      * Define API Endpoints.
      *
      *    Veneer\API::define( '/merchant-feed/google', array( 'CDO\Application\API\MerchantFeed', 'compute' ) )
      *
-     * @param      $path
-     * @param null $handler
+     * @param       $path
+     * @param null  $handler
+     * @param array $args
      *
+     * @return array|void
      */
-    public static function define( $path, $handler = null ) {
+    public static function define( $path, $handler = null, $args = array() ) {
 
-      if( !is_callable( $handler ) ) {
-        add_action( 'wp_ajax_' . $path, array( 'UsabilityDynamics\Veneer\API', 'default_handler' ) );
+      if( $handler && is_array( $handler ) && !$args ) {
+        $_args = $handler;
+      } else {
+        $_args = $args;
+      }
+
+      $_args = Utility::parse_args( $_args, array(
+        'path' => $path,
+        'method' => 'GET',
+        'handler' => $handler,
+        'namespace' => self::$namespace,
+        'scopes' => array(),
+        'parameters' => array()
+      ));
+
+      if( !is_callable( $_args->handler ) ) {
         return _doing_it_wrong( 'UsabilityDynamics\Veneer\API::define', 'Handler not callable.', null );
       }
 
-      add_action( 'wp_ajax_' . $path, $handler );
-      add_action( 'wp_ajax_nopriv_' . $path, $handler );
+      $_route = array(
+        '_type' => 'route',
+        'path' => self::get_path( $path, $_args ),
+        'namespace' => $_args->namespace,
+        'method' => $_args->method,
+        'url' => add_query_arg( array( 'action' => self::get_path( $path, $_args ) ), admin_url( 'admin-ajax.php' ) ),
+        'parameters' => $_args->parameters,
+        'scopes' => $_args->scopes,
+        'detail' => array(
+          'handler' => is_array( $_args->handler ) ? join( '::', $_args->handler ) : $_args->handler,
+          'action' => current_action()
+        )
+      );
+
+      add_action( 'wp_ajax_' . self::get_path( $path, $_args ), $_args->handler );
+      add_action( 'wp_ajax_nopriv_' . self::get_path( $path, $_args ), $_args->handler );
+
+      self::$_routes[] = $_route;
+
+      return $_route;
+
+    }
+
+    /**
+     * List Routes
+     *
+     * @param array $args
+     *
+     * @return array
+     */
+    public static function routes( $args = array() ) {
+
+      // Filter.
+      $args = (object) Utility::extend( array(), $args);
+
+      return API::$_routes;
 
     }
 
@@ -97,6 +167,18 @@ namespace UsabilityDynamics {
 
         return wp_send_json( $data );
       }
+
+    }
+
+    /**
+     * @param $path
+     * @param $args
+     *
+     * @return mixed|void
+     */
+    public static function get_path( $path, $args ) {
+
+      return apply_filters( 'usabilitydynamics::api::get_path', str_replace( '//', '/', ( '/' . ( $args->namespace ? $args->namespace . '/' : '' ) . $path ), $args ) );
 
     }
 
